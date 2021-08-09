808893168240513d956ae15dbea2446812907d95
Gillian Baker <gillianbaker97@gmail.com>
2021-08-09 10:28:04 -0400

    defining foreign keys

--- schema.sql
+++ schema.sql
@@ -23,7 +23,8 @@ CREATE TABLE role (
     id INT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (id)
 );
 
 CREATE TABLE employee (
@@ -31,6 +32,7 @@ id INT PRIMARY KEY,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
-manager_id INT
+manager_id INT,
+FOREIGN KEY (id)
 );
 
