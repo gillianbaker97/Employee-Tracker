3766f36cbf698af301a8a0aba1a86dddf0a4c55a
Gillian Baker <gillianbaker97@gmail.com>
2021-08-09 10:42:13 -0400

    refreshing the foreign key syntax

--- schema.sql
+++ schema.sql
@@ -15,7 +15,7 @@ USE tracker_db;
 
 CREATE TABLE department (
     id INT PRIMARY KEY,
-    name VARCHAR (30) NOT NULL
+    dept_name VARCHAR (30) NOT NULL
 
 );
 
@@ -24,7 +24,9 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (id)
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -33,6 +35,7 @@ first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
 manager_id INT,
-FOREIGN KEY (id)
+FOREIGN KEY (role_id)
+REFERENCES role(id)
 );
 
